ebd05722c6ed2f876aae4d2e291c6b52c48399fe
MailBIT Sales <yuvalleberstein21@gmail.com>
2024-07-27 11:56:50 +0300

    update changes

--- client/src/pages/AppRoutes.jsx
+++ client/src/pages/AppRoutes.jsx
@@ -10,7 +10,7 @@ import Footer from '../components/Footer'
 const AppRoutes = () => {
     return (
         <>
-            <section className={`section_banner bg-[url('./assets/20251948_6227820.webp')]`}>
+            <section className="section_banner bg-fade-in">
                 <header>
                     <Header />
                 </header>
