b20c9da12182a38f19fe630d3af2148367a7d800
MailBIT Sales <yuvalleberstein21@gmail.com>
2024-12-02 21:24:55 +0200

    update

--- client/src/pages/Resume.jsx
+++ client/src/pages/Resume.jsx
@@ -1,6 +1,4 @@
-import React from 'react'
-import Header from '../components/Header'
-import ResumeCV from '../assets/ResumeCV.jpeg'
+import ResumeCV from '../assets/ResumeCV.png'
 import { Link } from 'react-router-dom'
 
 const Resume = () => {
